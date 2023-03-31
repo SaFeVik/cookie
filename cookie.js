e5e2e773d0f616cd4a97e031c1c80e6dcaaa1137
Felix Vik <felixhusbanan@gmail.com>
2023-03-31 19:02:00 +0200

    siste?

--- cookie.js
+++ cookie.js
@@ -1,8 +1,10 @@
+// Henter alle stasjonære objekter fra DOM
 let handsEl = document.querySelector("#hands")
 let titleEl = document.querySelector("#title")
 let cpsItemsEl = document.querySelector("#cpsItems")
 let cookieEl = document.querySelector("#cookie")
 let cookieCounterEl = document.querySelector("#cookieCounter")
+let cookieDivEl = document.querySelector("#cookieDiv")
 let cpsCounterEl = document.querySelector("#cpsCounter")
 let upgradeItemEl = document.querySelector(".upgradeItem")
 let priceUEl = document.querySelector(".priceU")
@@ -10,20 +12,20 @@ let levelNrUEl = document.querySelector(".levelNrU")
 let shopTitleEl = document.querySelector("#shopTitle")
 let shopEl = document.querySelector("#shop")
 let mainEl = document.querySelector("main")
-let bodyEl = document.querySelector("body")
-console.log(mainEl)
 
+// Legger til klassen "show" hos #shop og main når man trykker på #shopTitle
 shopTitleEl.onclick = function(){
     shopEl.classList.toggle('show')
     mainEl.classList.toggle('show')
-    bodyEl.classList.toggle('show')
 }
 
+// Gjør await sleep(ms) funksjonelt
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-titleEl.onclick = function(){
+// Resetter localstorage når man trykker på tittelen
+/* titleEl.onclick = function(){
     localStorage.removeItem('hender')
     localStorage.removeItem('cps')
     localStorage.removeItem('lvlArr')
@@ -32,8 +34,8 @@ titleEl.onclick = function(){
     localStorage.removeItem('priceU')
     localStorage.removeItem('lvlU')
     localStorage.removeItem('tap')
-}
-cookieEl.onclick = addCookie
+} */
+
 
 // Localstorage henting
 let hender = []
@@ -44,29 +46,24 @@ let cps = 0
 if(localStorage.cps){
     cps = JSON.parse(localStorage.cps)
 }
-let priceU = 5000
+let priceU = 1000
 if(localStorage.priceU){
     priceU = JSON.parse(localStorage.priceU)
 }
 priceUEl.innerHTML = `${priceU}`
-
 let lvlU = 0
 if(localStorage.lvlU){
     lvlU = JSON.parse(localStorage.lvlU)
 }
 levelNrUEl.innerHTML = `${lvlU}`
-
 let tap = 1
 if(localStorage.tap){
     tap = JSON.parse(localStorage.tap)
 }
-
-cpsCounterEl.innerHTML = `Cps: ${cps.toFixed(1)}`
 let cookieCounter = 0
 if(localStorage.cookieCounter){
     cookieCounter = JSON.parse(localStorage.cookieCounter)
 }
-cookieCounterEl.innerHTML = `${Math.floor(cookieCounter)} Cookies!`
 
 let priceArr = []
 let lvlArr = []
@@ -85,18 +82,27 @@ for(let i = 0; i < hender.length; i++){
     handsEl.innerHTML += hender[i]
 }
 
+// Oppdaterer cps- og cookieCounter
+cpsCounterEl.innerHTML = `Cps: ${cps.toFixed(1)}`
+cookieCounterEl.innerHTML = `${Math.floor(cookieCounter)} Cookies!`
+
 // Hender rundt cookien
 function newHand(){
+    // Passer på at det ikke blir mer enn 81 hender
     if(antallHender < 81){
+        // Lager en ny hånd
         let handEls = document.querySelectorAll('.hand')
+        // Tar bort trykkes klassen fra alle hender slik at de ikke setter seg fast i trykket posisjon
         for(let i = 0; i < handEls.length; i++){
             handEls[i].classList.remove('trykkes')
         }
-        antallHender += 1
-        console.log(antallHender)
+        // Øker antall hender med 1
+        antallHender++
+        // Lager en ny hånd med klassen "hand"
         let imgEl = document.createElement('img')
         imgEl.src = "./bilder/arm.png"
         imgEl.classList.add('hand')
+        // Roterer og legger til klasser avhengig av om de er i første eller andre skall
         if(antallHender <= 36){
             imgEl.classList.add('hand1')
             imgEl.style.transform = `rotate(${antallHender*10}deg)`
@@ -105,40 +111,53 @@ function newHand(){
             imgEl.style.transform = `rotate(${antallHender*8}deg)`
         }
 
+        // Legger til hånden i handsEl, legger til hånden i hender-arrayen og lagrer de i localstorage
         handsEl.innerHTML += imgEl.outerHTML
         hender.push(imgEl.outerHTML)
         localStorage.hender = JSON.stringify(hender)
     }
 }
 
+// Aktiverer trykke animasjon hver 300ms
 setInterval(trykk, 300)
+// Deklarerer variabelen trykkes
 let trykkes = 0
 async function trykk(){
+    // Sjekker om det er hender rundt cookien
     if(hender.length > 0){
+        // Henter alle hender og legger til trykkes klassen på den som skal trykkes
         let handEls = document.querySelectorAll('.hand')
         if(handEls[trykkes]){
             handEls[trykkes].classList.add('trykkes')
             await sleep(150)
             handEls[trykkes].classList.remove('trykkes')
         }
+        // Teller fra 0 til antall hender og starter på nytt
         trykkes = (trykkes + 1) % hender.length
     }
 }
 
 // Lager shop items
 
+// Henter objekter fra json-fil
 fetch("cpsItems.json")
     .then(res => res.json())
     .then(cpsItems => {
+        // Kaller på makeItems-funksjonen med cpsItems som parameter
         makeItems(cpsItems)
     })
 
 function makeItems(cpsItems){
+    // Itererer gjennom cpsItems-arrayen og legger til html-elementer med informasjon fra objektene
     for(let i=0; i<cpsItems.length; i++){
-        priceLvlMaker()
+        // Lager default arrays med pris og level
         priceArr.push(cpsItems[i].price)
         lvlArr.push(0)
+        // Henter pris og level fra localstorage hvis det finnes
+        priceLvlMaker()
+        // item blir det aktelle kjøpeobjektet
         let item = cpsItems[i]
+        // Legger til kjøpeobjekter med informasjon fra json-filen og pris og level fra arrays
         cpsItemsEl.innerHTML += `
         <div class="cpsItem">
             <img src="./bilder/${item.image}">
@@ -161,29 +180,36 @@ function makeItems(cpsItems){
             </div>
         </div>
         `
-        console.log(lvlArr[i])
     }
 
     // Endrer cps, lvl og pris på itemet du kjøper
     for(let i=0; i<cpsItems.length; i++){
         let item = cpsItems[i]
+        // Oppdaterer hentingen av aktuelle html-elementer
         let itemEl = document.querySelectorAll('.cpsItem')[i]
         let lvlNrEl = document.querySelectorAll('.levelNr')[i]
         let priceEl = document.querySelectorAll('.price')[i]
+        // Når du trykker på kjøpeobjekt kjøres denne funksjonen
         itemEl.onclick = function(){
+            // Sjekker om du har nok cookies
             if(cookieCounter >= priceArr[i]){
-                console.log(priceArr[i])
+                // Trekker fra prisen på cookiecounteren
                 cookieCounter -= priceArr[i]
+                // Legger til cps som man har kjøpt
                 cps += item.cps
+                // Øker level og pris
                 lvlArr[i] += 1
                 priceArr[i] = Math.round(priceArr[i] * 1.15)
+                // Oppdaterer html-elementene
                 lvlNrEl.innerHTML = lvlArr[i]
                 priceEl.innerHTML = priceArr[i]
                 cpsCounterEl.innerHTML = `Cps: ${cps.toFixed(1)}`
                 
+                // Lagrer pris- og levelarrays og cps i localStorage
                 localStorage.priceArr = JSON.stringify(priceArr)
                 localStorage.lvlArr = JSON.stringify(lvlArr)
                 localStorage.cps = JSON.stringify(cps)
+                // Hvis det er det første kjøpeobjektet kjøres newHand()
                 if(itemEl == document.querySelectorAll('.cpsItem')[0]){
                     newHand()
                 }
@@ -192,29 +218,80 @@ function makeItems(cpsItems){
     }
         
 }
-
-function addCookie(){
+// addCookie kjører når man trykker på cookien
+cookieEl.onclick = addCookie
+async function addCookie(e){
+    // Aktiverer tapInfo med hendelsesobjektet
+    tapInfo(e)
+    // Skalerer cookien ned
+    cookieEl.style.scale = "0.97"
+    // Legger til så mange cookies som hvert trykk skal gi
     cookieCounter += tap
+    // Oppdaterer cookies telleren
     cookieCounterEl.innerHTML = `${Math.floor(cookieCounter)} Cookies!`
+    // Lagrer cookies telleren i localStorage
     localStorage.cookieCounter = JSON.stringify(cookieCounter)
+    // Venter 50ms før cookien skal skales opp igjen
+    await sleep(50)
+    cookieEl.style.scale = "1"
 }
 
+// Deklarerer variablen som holder styr på manuell clicks per second
+let clickCPS = 0
+
+// Skjer når man klikker på cookien
+async function tapInfo(e){
+    // Legger til i manuell clicks per second
+    clickCPS++
+    // Oppdaterer cps telleren med alle typer cookies per second
+    cpsCounterEl.innerHTML = `Cps: ${(cps+clickCPS*tap).toFixed(1)}`
+    // Lager p elementet som sier hvor mye cookies du får per klikk
+    let tapInfoEl = document.createElement('p')
+    // Legger til klassen "tapInfo" hos tapInfoEl
+    tapInfoEl.classList.add('tapInfo')
+    // Legger til teksten som sier hvor mye cookies du får per klikk
+    tapInfoEl.innerHTML = `+${tap}`
+    // Setter posisjonen til tapInfoEl ved musens posisjon
+    tapInfoEl.style.left = `${e.offsetX-10}px`
+    tapInfoEl.style.top = `${e.offsetY-25}px`
+    // Legger til tapInfoEl i cookieDivEl
+    cookieDivEl.appendChild(tapInfoEl)
+    // Venter 1 sekund før tapInfoEl skal fjernes for å unngå at det blir for mange og holder styr på manuell clicks per second
+    await sleep(1000)
+    // Fjerner 1 fra manuell clicks per second
+    clickCPS--
+    // Oppdaterer cps telleren med alle typer cookies per second
+    cpsCounterEl.innerHTML = `Cps: ${(cps+clickCPS*tap).toFixed(1)}`
+    // Fjerner tapInfoEl fra cookieDivEl
+    cookieDivEl.removeChild(tapInfoEl)
+}
+
+// Aktiverer addCps hvert 100ms
 setInterval(addCps, 100)
 function addCps(){
+    // Legger til så mange cookies som cps skal gi delt på 10 slik at den gir riktig mengde per sekund
     cookieCounter += cps/10
+    // Oppdaterer cookies telleren
     cookieCounterEl.innerHTML = `${Math.floor(cookieCounter)} Cookies!`
+    // Lagrer cookies telleren i localStorage
     localStorage.cookieCounter = JSON.stringify(cookieCounter)
 }
 
 upgradeItemEl.onclick = function(){
+    // Hvis du har nok cookies kjøper du upgrade
     if(cookieCounter >= priceU){
-        console.log("kjøpt")
+        // Fjerner prisen fra cookies telleren
         cookieCounter -= priceU
+        // Dobler tap slik at hvert klikk gir dobbelt så mye cookies
         tap *= 2
+        // Øker level på upgrade
         lvlU += 1
-        priceU = Math.round(priceU * 2.5)
+        // Dobler prisen på upgrade
+        priceU = Math.round(priceU * 2)
+        // Oppdaterer level og price på upgrade
         levelNrUEl.innerHTML = lvlU
         priceUEl.innerHTML = priceU
+        // Lagrer tap, level og price på upgrade i localStorage
         localStorage.lvlU = JSON.stringify(lvlU)
         localStorage.priceU = JSON.stringify(priceU)
         localStorage.tap = JSON.stringify(tap)
